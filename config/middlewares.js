e3e1005d94bcead7350c4c495c207afdd2da847d
geraldo <contact@lafabriqueducode.fr>
2026-09-04 11:59:30 +0200

    feat: configure Strapi CORS with dynamic allowed origins from environment variables

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,3 +1,9 @@
+const allowedOrigins = [
+  process.env.FRONTEND_URL || "https://photodececile.lafabriqueducode.fr",
+  process.env.FRONTEND_PREVIEW_URL,
+  "http://localhost:3000",
+].filter(Boolean);
+
 module.exports = [
   "strapi::logger",
   "strapi::errors",
@@ -21,7 +27,15 @@ module.exports = [
       },
     },
   },
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: allowedOrigins,
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
